526ec4e391c87ea3efb6ffb0a42a18abfeeed4a8
Alli Sodeeq <sodeeqalli@gmail.com>
2025-04-14 19:40:00 +0100

    changes

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='Pattern'/>
+
+      <div className='Wrapper'>
+        <header>
+          <h1>
+          Find  <span className='text-gradient'> Movies </span> You'll Enjoy without a hassle
+            </h1>
+        </header>
+
+        </div>
+    
+    </main>
+  )
+}
+
+export default App
